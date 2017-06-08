5e1c49dfb0101a0558b11be002416069b7c03fb0
Derek Santos <derekhalo@gmail.com>
2017-06-08 14:51:51 -0400

    Live_GUI is now a class that will handle GUI related actions.

--- gui.py
+++ gui.py
@@ -4,32 +4,37 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
-figure = plt.figure()
-axis = figure.add_subplot(1,1,1)
+class Live_GUI():
+    def __init__(self): 
+        self.file_name = 'sensor_data/EA:13:A7:37:70:DE - Wed Jun  7 16:43:31 2017 - 64.csv'
+        self.time_duration_list = []
+        self.frequency_list = []
+        self.figure = plt.figure()
+        self.axis = self.figure.add_subplot(1,1,1)
 
-def animate(passedIn):
-     
-    graph_data = None
-    with open('EA:13:A7:37:70:DE - Wed Jun  7 16:43:31 2017 - 96.csv') as current_file:
-        graph_data = current_file.read() 
-    
+    def plot_data(self, passedIn):
+        with open(self.file_name,'r') as current_file:
+            graph_data = current_file.read()
 
-    lines = graph_data.split('\n')
+        lines = graph_data.split('\n')
 
-    time_duration_list = []
-    frequency_list = []
+        for line in lines:
+            if len(line) > 1:
+                time_duration, frequency = line.split(',')
+                print (time_duration, frequency) 
+                self.time_duration_list.append(time_duration)
+                self.frequency_list.append(frequency)
+         
+        #axis.clear()
+        self.axis.plot(self.time_duration_list, self.frequency_list)
+        self.time_duration_list.clear()
+        self.frequency_list.clear()
 
-    for line in lines:
-        if len(line) > 1:
-            time_duration, frequency = line.split(',')
-            time_duration_list.append(time_duration)
-            frequency_list.append(frequency)
+    def run(self):
+        ani = animation.FuncAnimation(self.figure, self.plot_data, interval = 1000)
+        plt.show()
 
 
-    #axis.clear()
-    axis.plot(time_duration_list, frequency_list)
-    
-
-ani = animation.FuncAnimation(figure, animate, interval = 1000)
-plt.show() 
-    
+if __name__ == '__main__':
+    gui = Live_GUI()
+    gui.run() 
