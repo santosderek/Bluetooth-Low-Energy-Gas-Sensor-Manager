4fa41256bb0999f14e9266ba8a928d0efcb803f2
Derek Santos <derekhalo@gmail.com>
2017-06-09 15:39:45 -0400

    GUI is now embedding matplotlib graph into a Tkinter window.
    Now importing tkinter in order to embed a matplotlib graph into a tkinter window. I have also split the the file into two classes that will grow over time of development. 
    - Graph_Container is now responsible for the creation and management of the matplotlib graphs.
    - Live_GUI is now responsible for managing the overall window creation and main "frame". 
    - Once a Live_GUI class is initialized, invoking the 'run' function will create the window and manage the back-end.

--- gui.py
+++ gui.py
@@ -1,40 +1,94 @@
-import matplotlib.pyplot as plt
+import tkinter as tk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import matplotlib.animation as animation
-from matplotlib import style
 
-style.use('fivethirtyeight')
+LARGE_FONT= ("Verdana", 12)
 
-class Live_GUI():
-    def __init__(self): 
-        self.file_name = 'sensor_data/EA:13:A7:37:70:DE - Wed Jun  7 16:43:31 2017 - 64.csv'
+class Live_GUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        # Initializing tk window and title
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, "Live Sensor GUI")
+
+        # Main frame where graphs and the such will be drawn
+        main_frame = tk.Frame(self)
+        main_frame.pack(side="top", fill="both", expand = True)
+        main_frame.grid_rowconfigure(0, weight=1)
+        main_frame.grid_columnconfigure(0, weight=1)
+
+        # Initializing the Graph_Container class which contains the graph that will be shown on screen
+        self.graph_frame = Graph_Container(main_frame, self, "sensor_data/temp.csv")
+        self.graph_frame.grid(row=0, column=0, sticky="nsew")
+        self.graph_frame.tkraise()
+
+    def run(self):
+        figure_animation = animation.FuncAnimation(self.graph_frame.graph_figure, self.graph_frame.plot_figure, interval = 1000)
+        self.mainloop()
+
+
+class Graph_Container(tk.Frame):
+
+    # Parent is the parent tk.Frame class when inherited
+    # Controller is the class initialized that will use this Graph_Page
+    def __init__(self, parent, controller, file_path):
+        self.file_path = file_path
         self.time_duration_list = []
         self.frequency_list = []
-        self.figure = plt.figure()
-        self.axis = self.figure.add_subplot(1,1,1)
 
-    def plot_data(self, passedIn):
-        with open(self.file_name,'r') as current_file:
+        # Defining Figure and Plot classes to use with matplotlib's animation class
+        self.graph_figure = Figure(figsize=(5,5), dpi=100)
+        self.graph_plot = self.graph_figure.add_subplot(1,1,1)
+
+        # Initalizing the Frame class's __init__ function 
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Sensor", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        # The canvas that will show the graph_figure in the window
+        canvas = FigureCanvasTkAgg(self.graph_figure, self)
+        # Write it on screen
+        canvas.show()
+        # Fill the graph to the whole screen
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        # Responsible for giving the toolbar on the bottom of the window
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    # Function that will be passed into the animation.FuncAnimation function
+    # in order to plot the data in real time.
+    def plot_figure(self, i):
+        # Opens the csv file that will be needed to plot the data
+        with open(self.file_path, 'r') as current_file:
             graph_data = current_file.read()
 
+        # Splits the data into a list made of string elements
         lines = graph_data.split('\n')
 
+        # Loops through each string element while spliting the element in order
+        # to get each pair of time_duration and frequency
         for line in lines:
             if len(line) > 1:
                 time_duration, frequency = line.split(',')
-                print (time_duration, frequency) 
                 self.time_duration_list.append(time_duration)
                 self.frequency_list.append(frequency)
-         
-        #axis.clear()
-        self.axis.plot(self.time_duration_list, self.frequency_list)
+
+        # Plot's the data into the current_plot class
+        # First passed variable are the x-axis variables
+        # Second passed variables are the y-axis variables
+        # Each are list data types
+        self.graph_plot.plot(self.time_duration_list, self.frequency_list)
+
+        # Clear each list to get read for the next set of data
         self.time_duration_list.clear()
         self.frequency_list.clear()
 
-    def run(self):
-        ani = animation.FuncAnimation(self.figure, self.plot_data, interval = 1000)
-        plt.show()
-
-
 if __name__ == '__main__':
-    gui = Live_GUI()
-    gui.run() 
+    app = Live_GUI()
+    app.run()
