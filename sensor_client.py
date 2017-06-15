4646a0cdc0fd80f411799069a904b34efdb8f386
Derek Santos <derekhalo@gmail.com>
2017-06-15 10:17:15 -0400

    Improvements to the GUI
    The GUI when opened will automatically start looking for nearby devices
    and listing them in a listbox element. There is now buttons underneath
    the listbox element that can be used to manipulate a Sensor_Client
    class. These include "Scan", "Change to Latest File", "Connect",
    "Disconnect", "Read Frequency", and "Read Resistance".
    
    Lastly, the GUI file is now split into their own Class files.

--- sensor_client.py
+++ sensor_client.py
@@ -1,20 +1,39 @@
 """
-Author: Derek Santos
 
-Hexadecimal values:
-    0x02: HVPOT
-    0x03: LVPOT
-    0x04: IC
-    0x05: Gate Time
+Hexadecimal Valued Access Codes:
+
+0x02: HVPOT
+0x03: LVPOT
+0x04: IC
+0x05: Gate Time
+
+
+
 """
 
 import pexpect
 import sys
+import os
 import struct
 import binascii
 from time import sleep, time, ctime
+import bluetooth.ble as bluetooth
 
+def ble_scan():
+    # Scan's for all BLE devices nearby for 5 seconds
 
+    try:
+        print ('Searching for devices...')
+        service = bluetooth.DiscoveryService()
+        nearby_devices = service.discover(5)
+        # Lists the devices found during the search
+        for address, name in nearby_devices.items():
+            print ('Name: {} - Address: {}'.format(name, address))
+        return nearby_devices
+
+    except RuntimeError:
+        print ('ERROR: Make sure you are root before scaning...')
+        return None
 class Sensor_Client():
     def __init__(self, ADDRESS):
 
@@ -31,10 +50,14 @@ class Sensor_Client():
             # Variable to check if we are connected to sensor
             self.connected = False
 
+            # Variable that will be set to true when trying to connect to sensor.
+            # Allows all other functions to be aware that we are trying to connect
+            # to the sensor.
             self.stop_connection_checking = False
 
-            # Variable to check if we are currently reading from sensor
-            self.reading_channels = False
+            # Variables to check if we are currently reading from sensor
+            self.reading_frequency = False
+            self.reading_resistance = False
 
             # Time variable to be used at the start of recording data
             self.start_time = None
@@ -58,10 +81,22 @@ class Sensor_Client():
             # Default HV value being used at the start of recording data
             self.hv = 0x50
 
+            self.frequency_average = None
+            self.frequency_list = []
+
+
+
         except Exception as e:
             raise e
 
-    def connect(self):
+    def change_address(self, address):
+        self.address = address
+        self.disconnect()
+        self.gtool = pexpect.spawnu('sudo gatttool -i hci0 -b {addr} -t random -I'.format(addr = self.address), timeout = 3)
+
+    # Function that attempts connection with the sensor
+    # connect_one argument is if you only want to try to connect once
+    def connect(self, connect_once = False ):
         # Since under __init__ has already spanwed the startup command
         # we can free-ly send the connect command to try to start a connection
 
@@ -70,7 +105,12 @@ class Sensor_Client():
         if self.connected:
             return
 
+        # If already connecting (possibily in another thread)
+        if self.stop_connection_checking:
+            return
+
         # While not connected then we must attempt connection until it connects again.
+        start_connecting_attempt_time = time()
         while not self.connected:
             # Sends the string 'connect' to gatttol
             self.gtool.sendline('connect')
@@ -82,24 +122,31 @@ class Sensor_Client():
                 index = self.gtool.expect(['Connection successful',
                                            '.*busy.*',
                                            '.*Too many levels of symbolic links.*',
-                                           '.*refused.*'])
+                                           '.*refused.*'], timeout = 3)
                 if index == 0:
                     self.connected = True
-                    self.change_hv_value(self.hv)
+                    #self.change_hv_value(self.hv)
                     self.stop_connection_checking = False
                 elif index == 1:
                     print ('Device busy. Trying again')
                 elif index == 2:
                     print ('Too many levels of symbolic links. Please restart device.')
                     self.set_to_disconnected()
+                    return 'Too many symbolic links'
                 elif index == 3:
                     self.set_to_disconnected()
                     print ('Connection refused. Try again')
 
+                if connect_once:
+                    return
+
             # If pexpect time's out then set everything to disconnected and try again.
             except pexpect.exceptions.TIMEOUT as timeout:
                 self.set_to_disconnected()
+                if connect_once:
+                    return
                 self.connect()
+
             # If there was an uknown error state the error and try again.
             except Exception as e:
                 print ('ERROR:', e)
@@ -109,7 +156,7 @@ class Sensor_Client():
     # correct data assignments
     def set_to_disconnected(self):
         self.connected = False
-        self.reading_channels = False
+        self.reading_frequency = False
         self.stop_connection_checking = False
 
     # Changes the hv value if the sensor is connected
@@ -121,10 +168,10 @@ class Sensor_Client():
             print ('Not connected')
             return
         # old reading channels value
-        old_rc_value = self.reading_channels
+        old_rc_value = self.reading_frequency
 
         # Stop attempting to read the values
-        self.reading_channels = False
+        self.reading_frequency = False
         # Stop trying to see if the connection is still alive
         # (We already know it is)
         self.stop_connection_checking = True
@@ -135,8 +182,8 @@ class Sensor_Client():
         self.gtool.sendline(self.char_write(0x02, 0x00, self.hv))
         # Sleep for 3 seconds to make sure the command was sent
         sleep(3)
-        # Set reading_channels to what it was before
-        self.reading_channels = old_rc_value
+        # Set reading_frequency to what it was before
+        self.reading_frequency = old_rc_value
         # Check connection to make sure it is connected
         self.check_connection()
         # If not connected re-establish the connection
@@ -146,55 +193,101 @@ class Sensor_Client():
         # Continue to check if the connection is dropped
         self.stop_connection_checking = False
 
-    def request_frequency(self):
+    def read_frequency(self):
+        if self.start_time is None:
+            self.start_time = time()
+        # For each channel being passed into the class
+        for channel in self.channels:
+            # Ready the sensor for being used
+            self.gtool.sendline(self.char_write(0x04, 0x80, channel + 0x80 + 0x0a))
+            self.gtool.sendline(self.char_write(0x04, 0x80, channel + 0x80 + 0x0b))
 
-        # While the sensor is being used
-        while not self.is_stopped:
-            try:
-                if self.reading_channels and self.connected:
-                    # We must record the starting time that the sensor is being started
-                    if self.start_time is None:
-                        self.start_time = time()
-                    # For each channel being passed into the class
-                    for channel in self.channels:
-                        # Ready the sensor for being used
-                        self.gtool.sendline(self.char_write(0x04, 0x80, channel + 0x80 + 0x0a))
-                        self.gtool.sendline(self.char_write(0x04, 0x80, channel + 0x80 + 0x0b))
+            # Send the sensor the gate time desired
+            self.gtool.sendline(self.gate_time_write(0x05, self.gate_time))
 
-                        # Send the sensor the gate time desired
-                        self.gtool.sendline(self.gate_time_write(0x05, self.gate_time))
+            # Sleep for the total time of the sensor's gate time
+            sleep(self.gate_time_delay)
+            sleep(self.gate_time / 1000.0)
 
-                        # Sleep for the total time of the sensor's gate time
-                        sleep(self.gate_time_delay)
-                        sleep(self.gate_time / 1000.0)
+            # Request the frequency data
+            self.gtool.sendline(self.char_write(0x04, 0x08, 0x04))
+            sleep(self.gate_time_delay)
 
-                        # Request the frequency data
-                        self.gtool.sendline(self.char_write(0x04, 0x08, 0x04))
-                        sleep(self.gate_time_delay)
+            # To get the Frequency we have to take out the word 'descriptor' with the space at the end
+            frequency = self.read_handle()
+            if frequency is None:
+                return
+            frequency = frequency.replace(' ','')
 
-                        # To get the Frequency we have to take out the word 'descriptor' with the space at the end
-                        frequency = self.read_handle()[len('descriptor: '):len('descriptor: ') + 11]
-                        frequency = frequency.replace(' ','')
+            # Data transfered is little-edian. Convert the HEX to ASCII then INT
+            count = struct.unpack('<I', binascii.unhexlify(frequency))[0]
 
-                        # Data transfered is little-edian. Convert the HEX to ASCII then INT
-                        count = struct.unpack('<I', binascii.unhexlify(frequency))[0]
+            # Formula to convert the count recieved from Hz to MHz
+            frequency = float(count) / float(2) / (self.gate_time / 1000.0) / 1000000.0
 
-                        # Formula to convert the count recieved from Hz to MHz
-                        frequency = float(count) / float(2) / (self.gate_time / 1000.0) / 1000000.0
+            # If the list of frequency data that we have has values within itself
+            if len(self.frequency_list) > 0:
+                # Check if the frequency is within acceptable range
 
-                        # Append this value and the current time duration to a file
-                        # The file name contains the MAC Address, Date/time, and channel
-                        with open('sensor_data/{0} - {1} - {2:02d}.csv'.format(self.address, ctime(self.start_time), channel), 'a') as current_file:
-                            data = '{0:5.1f},{1:1.8f}\n'.format(time() - self.start_time, float (frequency))
-                            current_file.write(data)
+                if self.check_frequency(frequency):
+                    # Append if acceptable
+                    self.frequency_list.append(frequency)
+                # If not acceptable then skip writing it to the file
+                else:
+                    continue
 
-                elif (not self.stop_connection_checking) and (self.connected):
-                    # To check if we are still connected we can read from the sensor
-                    self.check_connection()
+            else:
+                # If it is the first element then automatically put it within the file
+                # And append it to the frequency_list
+                self.frequency_list.append(frequency)
 
-            except Exception as e:
-                #print (e)
-                pass
+            # Append this value and the current time duration to a file
+            # The file name contains the MAC Address, Date/time, and channel
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            with open(dir_path + '/sensor_data/{0} - {1} - {2:02d} - Frequency.csv'.format(self.address, ctime(self.start_time), channel), 'a') as current_file:
+                data = '{0:5.1f},{1:1.8f}\n'.format(time() - self.start_time, float (frequency))
+                current_file.write(data)
+
+    def read_resistance(self):
+        if self.start_time is None:
+            self.start_time = time()
+        self.gtool.sendline(self.char_write(0x04))
+        digital_value = self.read_handle()
+        if digital_value is None:
+            return
+        digital_value = digital_value.replace(' ', '')
+        digital_value = struct.unpack('<I', binascii.unhexlify(digital_value))[0]
+
+        # dlog = Digital Value
+        # vref = Voltage value
+        #
+        voltage_ref = 3.3
+        vlsb = voltage_ref / 1023.0
+        vlog = digital_value * vlsb
+        vc = 5
+        rl = 2.2
+
+        resistance = (vc - vlog) * rl / vlog
+        time_duration = time() - self.start_time
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+        with open(dir_path + '/sensor_data/{0} - {1} - {2}.csv'.format(self.address, ctime(self.start_time), 'Resistance'), 'a') as current_file:
+            data = '{0:5.1f},{1:5d},{2:5.3f},{3:5.3f}\n'.format(time_duration, digital_value, voltage_ref, float(resistance))
+            current_file.write(data)
+
+    # To calculate the frequency_average we take the average of frequency_list
+    # But it will only do the latest 5 items
+    def calculate_frequency_average(self):
+        if len(self.frequency_list) > 5:
+            self.frequency_list = self.frequency_list[len(self.frequency_list) - 5:]
+        self.frequency_average = sum(self.frequency_list) / len(self.frequency_list)
+
+    # This function will return True or False if the frequency is within the allowed range
+    def check_frequency(self, frequency):
+        self.calculate_frequency_average()
+        lower_limit = float(self.frequency_average) - 0.2
+        upper_limit = float(self.frequency_average) + 0.2
+        return lower_limit <= frequency and frequency <= upper_limit
 
     # Python does not do function overloading
     def char_write(self, first_int, second_int = None, third_int = None):
@@ -222,7 +315,7 @@ class Sensor_Client():
             # else it's disconnected; reconnect and try again
             if value == 0:
                 if not self.gtool.after == '\'NoneType\' object is not subscriptable':
-                    return self.gtool.after
+                    return self.gtool.after[len('descriptor: '):len('descriptor: ') + 11]
             elif value == 1:
                 self.connect()
                 self.read_handle()
@@ -249,11 +342,31 @@ class Sensor_Client():
         except Exception as e:
             self.connected = False
 
-    def disconnect(self):
-        self.is_stopped = True
-        self.gtool.sendline('exit')
-
     # The primary function to be multithreaded
     # This is the general flow for the progam
     def run(self):
-        self.request_frequency()
+
+        # While the sensor is being used
+        while not self.is_stopped:
+            try:
+                if self.reading_frequency and self.connected:
+                    # We must record the starting time that the sensor is being started
+                    self.read_frequency()
+
+                elif self.reading_resistance and self.connected:
+                    self.read_resistance()
+
+                elif not self.stop_connection_checking and \
+                     not self.reading_resistance and \
+                     not self.reading_frequency and self.connected:
+                    # To check if we are still connected we can read from the sensor
+                    self.check_connection()
+
+            except Exception as e:
+                print (e)
+                #pass
+
+    def disconnect(self):
+        self.set_to_disconnected()
+        sleep(1)
+        self.gtool.sendline('exit')
--- sensor_manager.py
+++ sensor_manager.py
@@ -1,12 +1,13 @@
 from threading import Thread
-from sensor_client import * 
+from sensor_client import *
 
 class Sensor_Manager():
     def __init__(self):
         # Creating the sensor_client list
         self.sensor_list = []
-        # Creating a list for threads 
-        self.thread_list = []
+        # Creating a list for threads
+        self.thread_list_frequency = []
+        #self.thread_list_resistance = []
 
     def add_device(self, address):
         # Checks to see if device MAC_Address is already in the list
@@ -15,7 +16,7 @@ class Sensor_Manager():
             if sensor.address == address:
                 same_mac_address_found = True
         if same_mac_address_found:
-            return 
+            return
         # Creation of the Sensor_Client object by passing in a MAC Address
         new_sensor = Sensor_Client(address)
 
@@ -23,37 +24,43 @@ class Sensor_Manager():
         self.sensor_list.append(new_sensor)
 
         # Knowing that lists stay in order, we can involke it's run function into a thread
-        new_thread = Thread(target = self.sensor_list[len(self.sensor_list) - 1].run, args = ())
+        new_thread_frequency = Thread(target = self.sensor_list[len(self.sensor_list) - 1].run, args = ())
 
         # The thread will act as a daemon
         # When the main thread closes, all daemon threads will as well
-        new_thread.daemon = True
+        new_thread_frequency.daemon = True
 
-        # The new thread object gets sent the the back of the list and will start
-        self.thread_list.append(new_thread)
-        self.thread_list[len(self.thread_list) - 1].start()
+        # The new frequency thread object gets sent the the back of the list and will start
+        self.thread_list_frequency.append(new_thread_frequency)
+        self.thread_list_frequency[len(self.thread_list_frequency) - 1].start()
 
-    
+        ## The new resistance thread object gets sent the the back of the list and will start
+        #new_thread_resistance = Thread(target = self.sensor_list[len(self.sensor_list) - 1].request_resistance, args = ())
+        #new_thread_resistance.daemon = True
+        #self.thread_list_resistance.append(new_thread_resistance)
+        #self.thread_list_resistance[len(self.thread_list_resistance) - 1].start()
+
     def list_devices(self):
         # For ever sensor_client in the sensor list, we will print it's information
         for position, device in enumerate(self.sensor_list):
-            print ('{} - Sensor Address: {}, Connected: {}, Hv Value: {}, Hv Hex: {}, Read: {}'.format(position + 1,
+            print ('{} - Sensor Address: {}, Connected: {}, Hv Value: {}, Hv Hex: {}, Reading fequency: {}, Reading Resistance: {}'.format(position + 1,
                                                                                device.address,
                                                                                device.connected,
                                                                                device.hv,
                                                                                hex(device.hv),
-                                                                               device.reading_channels))
+                                                                               device.reading_frequency,
+                                                                               device.reading_resistance))
     # Given a position on the sensor_list, this function will change the hv value of the sensor
     def change_hv_value(self, pos, value):
         self.sensor_list[pos].change_hv_value(value)
-        
-        
+
     def remove_device(self, pos):
         # Involkes the disconnect function of sensor_client
         self.sensor_list[pos].disconnect()
 
         # Removes the thread and sensor objects from their respective lists
-        self.thread_list.remove(self.thread_list[pos])
+        self.thread_list_frequency.remove(self.thread_list_frequency[pos])
+        #self.thread_list_resistance.remove(self.thread_list_resistance[pos])
         self.sensor_list.remove(self.sensor_list[pos])
 
     def remove_all_devices(self):
@@ -61,32 +68,48 @@ class Sensor_Manager():
             sensor.disconnect()
             self.sensor_list.remove(sensor)
 
-        for thread in self.thread_list:
-            self.thread_list.remove(thread)
+        for thread in self.thread_list_frequency:
+            self.thread_list_frequency.remove(thread)
+
+        #for thread in self.thread_list_resistance:
+        #    self.thread_list_resistance.remove(thread)
 
     def connect_device(self, pos):
-        try: 
+        try:
             self.sensor_list[pos].connect()
         except Exception as e:
-            
             print ('ERROR:', e)
-        
-    def start_device(self, pos):
-        # If device hasn't already started reading
-        # reading_channels will be set as true
-        if not self.sensor_list[pos].reading_channels: 
-            self.sensor_list[pos].reading_channels = True
 
-    def stop_device(self, pos):
-        if self.sensor_list[pos].reading_channels:
-            self.sensor_list[pos].reading_channels = False
+    # Starting device reading
+    def start_device_frequency(self, pos):
+        # If device hasn't already started reading
+        # reading_frequency will be set as true
+        if not self.sensor_list[pos].reading_frequency:
+            self.sensor_list[pos].reading_frequency = True
+
+    def start_device_resistance(self, pos):
+        # If device hasn't already started reading
+        # reading_frequency will be set as true
+        if not self.sensor_list[pos].reading_resistance:
+            self.sensor_list[pos].reading_resistance = True
+
+    #Stop device reading
+    def stop_device_frequency(self, pos):
+        if self.sensor_list[pos].reading_frequency:
+            self.sensor_list[pos].reading_frequency = False
+
+    def stop_device_resistance(self, pos):
+        if self.sensor_list[pos].reading_resistance:
+            self.sensor_list[pos].reading_resistance = False
 
     def start_all_devices(self):
          # Sets all devices in the sensor_list to start reading values
         for sensor in self.sensor_list:
-            sensor.reading_channels = True
+            sensor.reading_frequency = True
+            sensor.reading_resistance = True
 
     def stop_all_devices(self):
         # Sets all devices in the sensor_list to stop reading values
         for sensor in self.sensor_list:
-            sensor.reading_channels = False
+            sensor.reading_frequency = False
+            sensor.reading_resistance = False
